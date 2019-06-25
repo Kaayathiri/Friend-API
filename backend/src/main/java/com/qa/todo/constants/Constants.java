09973971ea410bd9c700f7113758e58cd413f96c
s-h-a-n-k-s <ramsdentom@gmail.com>
2019-06-25 10:15:35 +0100

    Update FRIEND url

--- backend/src/main/java/com/qa/todo/constants/Constants.java
+++ backend/src/main/java/com/qa/todo/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.qa.todo.constants;
 
 public class Constants {
-	public static final String FRIEND = "/friend";
+	public static final String FRIEND = "/friends";
 	public static final String FRIEND_WITH_PARAM = "/friend/{id}";
 }
