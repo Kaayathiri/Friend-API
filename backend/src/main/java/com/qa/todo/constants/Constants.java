db3f001e6994a18b3edf0a022b590f6286d565e6
s-h-a-n-k-s <ramsdentom@gmail.com>
2019-06-25 09:53:54 +0100

    Update files

--- backend/src/main/java/com/qa/todo/constants/Constants.java
+++ backend/src/main/java/com/qa/todo/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.qa.todo.constants;
 
 public class Constants {
-	public static final String TODO = "/todo";
-	public static final String TODO_WITH_PARAM = "/todo/{id}";
+	public static final String FRIEND = "/friend";
+	public static final String FRIEND_WITH_PARAM = "/friend/{id}";
 }
--- backend/src/main/java/com/qa/todo/persistence/FriendRepository.java
+++ backend/src/main/java/com/qa/todo/persistence/FriendRepository.java
@@ -3,9 +3,9 @@ package com.qa.todo.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.qa.todo.domain.Todo;
+import com.qa.todo.domain.Friend;
 
 @Repository
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface FriendRepository extends JpaRepository<Friend, Long> {
 	
 }
